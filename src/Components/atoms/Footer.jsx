9c91f0a451dbe981e367c9d7fc579b89fa80be6c
unknown <cristiangvramos1313@gmail.com>
2023-03-22 21:49:34 -0600

    [FIX]+deploy vuelto a configurar

--- src/Components/atoms/Footer.jsx
+++ src/Components/atoms/Footer.jsx
@@ -6,28 +6,48 @@ import Programar from "../../assets/img/programacion.png"
 import Dibujar from "../../assets/img/paleta-de-color.png"
 
 function Footer() {
-    return (    
-        <>
-        <div><center><p>Aficiones</p></center></div>
-        <div className="footer">
-            <div >
-                <h1>{data.aficiones.af1}</h1>
-                <div className="aficion"><img src={Musica} alt="escuchar musica" className="aficion" /></div>
-            </div>
-            <div><h1>{data.aficiones.af2}</h1>
-                <div className="aficion"><img src={Viajar} alt="viajar" className="aficion" /></div>
-            </div>
-            <div><h1>{data.aficiones.af3}</h1>
-                <div className="aficion"><img src={Programar} alt="programar" className="aficion" /></div>
-            </div>
-            <div><h1>{data.aficiones.af4}</h1>
-                <div className="aficion"><img src={Azar} alt="juegos de azar" className="aficion" /></div>
-            </div>
-                <div><h1>{data.aficiones.af5}</h1><div className="aficion"><img src={Dibujar} alt="dibujar" className="aficion" /></div>
-            </div>
+    return (
+      <>
+        <div>
+          <center>
+            <p>Aficiones</p>
+          </center>
         </div>
-        </>
-     );
+        <div className="footer">
+          <div>
+            <h1>{data.aficiones.af1}</h1>
+            <div className="aficion">
+              <img src={Musica} alt="escuchar musica" className="aficion" />
+            </div>
+          </div>
+          <div>
+            <h1>{data.aficiones.af2}</h1>
+            <div className="aficion">
+              <img src={Viajar} alt="viajar" className="aficion" />
+            </div>
+          </div>
+          <div>
+            <h1>{data.aficiones.af3}</h1>
+            <div className="aficion">
+              <img src={Programar} alt="programar" className="aficion" />
+            </div>
+          </div>
+          <div>
+            <h1>{data.aficiones.af4}</h1>
+            <div className="aficion">
+              <img src={Azar} alt="juegos de azar" className="aficion" />
+            </div>
+          </div>
+          <div>
+            <h1>{data.aficiones.af5}</h1>
+            <div className="aficion">
+              <img src={Dibujar} alt="dibujar" className="aficion" />
+            </div>
+          </div>
+        </div>
+
+      </>
+    );
 }
 
 export default Footer;
